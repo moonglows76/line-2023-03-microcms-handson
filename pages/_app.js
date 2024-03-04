54029981e4c748926deff1275ae1a547f43a3e52
Fuminori Mori <616572+moonglows76@users.noreply.github.com>
2024-03-04 01:53:45 +0000

    _app.jsにコメント追加

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,21 @@
 import "../styles/global.css";
 import { useEffect, useState, createContext } from "react";
 
+// liff.init で LIFF アプリの初期化を行い、各ページで利用できるようにしています。今回のハンズオンでは、プロフィール情報を試します。
 export const LiffContext = createContext({});
 
 export default function App({ Component, pageProps }) {
   // [liffObject, profile]
   const [[liffObject, profile], setLiffState] = useState([null, null]);
   useEffect(() => {
-    if (!pageProps.liffId) return;
+  if (!pageProps.liffId) return;
     import("@line/liff").then((liff) => {
       liff
         .init({ liffId: pageProps.liffId })
         .then(() => {
           if (liff.isLoggedIn()) {
+            // ログインしている場合は、liff.init の情報と プロフィールを、liffState を更新します。
+            // プロフィール情報の取得は liff.profile の API を利用しています。
             // プロフィール情報の取得をする
             liff
               .getProfile()
@@ -23,6 +26,7 @@ export default function App({ Component, pageProps }) {
                 console.warn({ err })
               })
           } else {
+            // ログインしていない場合は、liff.init の情報のみを liffState に更新しています。
             setLiffState([liff, null]);
           }
         })
